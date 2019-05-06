3aa1ee058744b729cce01de4005ff83aefb2f76b
stefren <stefren@mit.edu>
2019-05-06 12:17:02 -0400

    flip video horizontally

--- process_video.sh
+++ process_video.sh
@@ -3,6 +3,7 @@
 ffmpeg -i $1 -vf scale=640:480 temp.mp4
 ffmpeg -i temp.mp4 -vf pad=1280:960:320:240 temp2.mp4
 ffmpeg -i temp2.mp4 -vf perspective=200:200:1080:200:0:960:1280:960:sense=1 temp3.mp4
-ffmpeg -i temp3.mp4 -vf scale=640:480 final_output.mp4
+ffmpeg -i temp3.mp4 -vf scale=640:480 temp4.mp4
+ffmpeg -i temp4.mp4 -vf hflip -c:a copy final_output.mp4
 rm temp*.mp4
 scp final_output.mp4 debian@beaglebone.local:~/video/
